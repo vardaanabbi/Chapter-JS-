4918f7830c81d705680a41e093a887b73737889d
Vardaan <vardaanabbi18@gmail.com>
2026-05-30 00:01:01 +0530

    animation created and java script practice

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert("namaste")
+// console.log("kya haal hai ?")
+// document.title = "Chapter JS"
+// document.body.style.backgroundColor = "black"
+
+let b = 6
+let c = 8 
+console.log(b+c)
+
+let e = 5 ;
+let f = 7 ;
+let d = e>f ? (e-f) : (f-e) ;
+console.log(d)
