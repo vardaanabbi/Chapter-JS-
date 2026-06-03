95bd000135ec146de8b2fcbe1d1a18f5b3f6045c
Vardaan <vardaanabbi18@gmail.com>
2026-06-03 15:30:27 +0530

    removed unwanted comments

--- factorial.js
+++ factorial.js
@@ -1,12 +1,12 @@
 // calculating factorial of a number using reduce and for loops . 
 
-// let n = Number(prompt("Enter the number"))
-// let array = []
-// for ( let i = 1 ; i < n+1 ;  i ++ ) {array.push(i)}
+let n = Number(prompt("Enter the number"))
+let array = []
+for ( let i = 1 ; i < n+1 ;  i ++ ) {array.push(i)}
 
-// console.log(array)
-// const func = (e,f)=> {return e*f}
-// alert((array.reduce(func)))
+console.log(array)
+const func = (e,f)=> {return e*f}
+alert((array.reduce(func)))
 
 function fact(n){
     let result = 1 ;
