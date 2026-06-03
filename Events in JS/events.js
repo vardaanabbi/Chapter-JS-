62ccf72c8b53a59efa156338418899e5ef6f2b38
Vardaan <vardaanabbi18@gmail.com>
2026-06-03 20:26:39 +0530

    completed events topic in JS

---
+++ Events in JS/events.js
@@ -0,0 +1,11 @@
+// let button = document.getElementById("btn")
+// button.addEventListener("click" , ()=> {
+//     document.querySelector(".box").innerHTML = "<b> I was clicked </b>"
+// })
+
+// button.addEventListener("keydown" , (e)=> {
+//     console.log(e.key)})
+
+console.log("hello")
+setInterval(()=> {console.log("jai mata di")} , 2)
+console.log("end")
